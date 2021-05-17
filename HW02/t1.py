a7b5843775cc03dbbc3dcf94827f26d566cca21b
Vadim S <svershchevskiy.vadim@gmail.com>
2021-05-17 22:37:59 +0300

    added counter

--- HW02/t1.py
+++ HW02/t1.py
@@ -5,15 +5,16 @@ a = int(input("Введите сторону a "))
 b = int(input("Введите сторону b "))
 
 
-def break_into_squares(a, b):
+def break_into_squares(a, b, count=0):
     if a == 0 or b == 0:
+        print("Всего ", count)
         return
     if a > b:
         print("Квадрат со стороной ", b, " и ", b)
-        break_into_squares(a - b, b)
+        break_into_squares(a - b, b, count + 1)
     else:
         print("Квадрат со стороной ", a, " и ", a)
-        break_into_squares(a, b - a)
+        break_into_squares(a, b - a, count + 1)
 
 
 break_into_squares(a, b)
